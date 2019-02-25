8fe35dd732b089e62783dcaf3b49ecf1e4cebfe0
Grovesy101 <99tgroves@gmail.com>
2019-02-25 23:41:09 +0000

    Added task 3

---
+++ Task 3/com/grovesy/Task3.java
@@ -0,0 +1,125 @@
+package com.grovesy;
+
+import static com.jogamp.opengl.GL.GL_POINTS;
+import static com.jogamp.opengl.GL2ES2.GL_FRAGMENT_SHADER;
+import static com.jogamp.opengl.GL2ES2.GL_VERTEX_SHADER;
+import static com.jogamp.opengl.GL2ES3.GL_COLOR;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.FloatBuffer;
+import java.util.Scanner;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+
+import com.jogamp.common.nio.Buffers;
+import com.jogamp.opengl.GL4;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLContext;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.awt.GLCanvas;
+
+public class Task3 extends JFrame implements GLEventListener {
+
+	private GLCanvas canvas;
+
+	private int[] vertexArrayObject = new int[1];
+	private int renderingProgram;
+
+	public Task3() {
+		setTitle("OpenGL");
+		setSize(600, 400);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		canvas = new GLCanvas();
+		canvas.addGLEventListener(this);
+		add(canvas);
+		setVisible(true);
+	}
+
+	@Override
+	public void display(GLAutoDrawable draw) {
+		GL4 gl = (GL4) GLContext.getCurrentGL();
+
+		FloatBuffer buffer = Buffers.newDirectFloatBuffer(new float[] { 0.f, 0.f, 0.f, 1.f });
+		gl.glClearBufferfv(GL_COLOR, 0, buffer);
+
+		gl.glUseProgram(renderingProgram);
+		gl.glPointSize(30f);
+		gl.glDrawArrays(GL_POINTS, 0, 1);
+	}
+
+	@Override
+	public void dispose(GLAutoDrawable draw) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(GLAutoDrawable draw) {
+		GL4 gl = (GL4) GLContext.getCurrentGL();
+		renderingProgram = createShaderProgram();
+		gl.glGenVertexArrays(vertexArrayObject.length, vertexArrayObject, 0);
+		gl.glBindVertexArray(vertexArrayObject[0]);
+		
+	}
+
+	private int createShaderProgram() {
+		GL4 gl = (GL4) GLContext.getCurrentGL();
+		String[] vShaderSource = readShaderSource("vertex.shader");
+		String[] fShaderSource = { "#version 430 \n", "out vec4 colour; \n", "void main(void)\n",
+				"{colour = vec4(0.0, 0.0, 1.0, 1.0);}", };
+		
+		int vertexShader = gl.glCreateShader(GL_VERTEX_SHADER);
+		gl.glShaderSource(vertexShader, vShaderSource.length, vShaderSource, null, 0); 
+		gl.glCompileShader(vertexShader);
+		
+		int fragmentShader = gl.glCreateShader(GL_FRAGMENT_SHADER);
+		gl.glShaderSource(fragmentShader, fShaderSource.length, fShaderSource, null, 0); 
+		gl.glCompileShader(fragmentShader);
+		
+		int vfprogram = gl.glCreateProgram();
+		gl.glAttachShader(vfprogram, vertexShader);
+		gl.glAttachShader(vfprogram, fragmentShader);
+		gl.glLinkProgram(vfprogram);
+		
+		gl.glDeleteShader(vertexShader);
+		gl.glDeleteShader(fragmentShader);
+		return vfprogram;
+	}
+
+	
+	private String[] readShaderSource(String filename) {
+		Vector<String> lines = new Vector<String>();
+		Scanner sc;
+		try {
+			sc = new Scanner(new File(filename));
+		}catch(IOException e) {
+			System.out.println("Error Reading File!");
+			return null;
+		}
+		
+		while(sc.hasNext()) {
+			lines.addElement(sc.nextLine());
+		}
+		
+		String[] program = new String[lines.size()];
+		for(int i = 0; i < lines.size(); i++) {
+			program[i] = (String) lines.elementAt(i) + "\n";
+		}
+		
+		return program;
+	}
+	
+	
+	@Override
+	public void reshape(GLAutoDrawable draw, int x, int y, int width, int height) {
+
+	}
+
+	public static void main(String[] args) {
+		new Task3();
+	}
+
+}
---
+++ Task 3/fragment.shader
@@ -0,0 +1,4 @@
+#version 430
+out vec4 colour;
+void main(void)
+{ colour = vec4(0.0, 0.0, 1.0, 1.0);}
